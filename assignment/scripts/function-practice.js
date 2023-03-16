ca93d9e363b052be8f5e899e10c4b9ae95afd353
mandi andrade <mandi.andrade20@gmail.com>
2023-03-15 21:48:54 -0500

    questions 2-4 completed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,23 +14,35 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(name) {
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
-
+console.log(helloName('Jo'));
+console.log(helloName('Stacy'));
+console.log(helloName('[your name here]'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+  let answer = firstNumber + secondNumber;
+  return 'The answer of ' + firstNumber + ' + ' + secondNumber + ' is ' + answer;
 }
 
+// call the function:
+console.log(addNumbers(1, 2));
+console.log(addNumbers(14, 5));
+console.log(addNumbers(432.3, 12.513));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree(num1, num2, num3){
+  let answer = num1 * num2 * num3;
+  return 'math problem: '+ num1+ ' X '+ num2+ ' X '+ num3+ ' = '+ answer;
+} // end multiplyThree
 
-}
-
+// call the function, tests:
+console.log(multiplyThree(1, 2, 3));
+console.log(multiplyThree(3, 6, 9));
+console.log(multiplyThree(1056, 56412, 643));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
